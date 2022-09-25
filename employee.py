9825f90a4d58a4ef673c1e154dfbe6aa6301d789
Josh <joshuasusak@gmail.com>
2022-09-25 12:03:14 +0100

    Add improved solution w/ abc module.

--- employee.py
+++ employee.py
@@ -1,20 +1,16 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+from abc import ABC, abstractmethod
 
 
-class Employee:
+class Employee(ABC):
     # Boolean for if the employee is on salary or hourly. Default is salary.
     def __init__(self, name, commissiontype=None):
 
         self.name = name
         self.commission = commissiontype
-        self.salamount = 0
-        self.employeetype = None
-        self.hourwage = 0
-        self.hours = 0
 
         self.bonusamount = 0
-
         self.contracts = 0
         self.contractprices = 0
 
@@ -36,47 +32,50 @@ class Employee:
         else:
             raise ValueError("Invalid commission types...")
 
-    def get_pay(self):
-        if self.salamount > 0:
-            return self.salamount + self.add_commission()
-        else:
-            return (self.hourwage * self.hours) + self.add_commission()
-
-    def __str__(self):
-        str = f"{self.name} works on "
-        if self.employeetype == "salary":
-            str += f"a monthly salary of {self.salamount}"
-        else:
-            str += f"a contract of {self.hours} hours at {self.hourwage}/hour"
-
-
-
+    def get_commission(self):
         if self.commission is None:
-            str += "."
+            return "."
         elif self.commission == "bonus":
-            str += f" and receives a bonus commission of {self.add_commission()}."
-        # Contract
+            return f" and receives a bonus commission of {self.add_commission()}."
+            # Contract
         else:
-            str += f" and receives a commission for {self.contracts} contract(s) at {self.contractprices}/contract."
+            return f" and receives a commission for {self.contracts} contract(s) at {self.contractprices}/contract."
 
-        str += f"  Their total pay is {self.get_pay()}."
+    @abstractmethod
+    def get_pay(self):
+        pass
 
-        return str
+    @abstractmethod
+    def __str__(self):
+        pass
 
 
 class SalaryEmployee(Employee):
     def __init__(self, name, salamount, commissiontype=None):
         super().__init__(name, commissiontype)
-        self.employeetype = "salary"
         self.salamount = salamount
+
+    def get_pay(self):
+        return self.salamount + self.add_commission()
+
+    def __str__(self):
+        message = f"{self.name} works on a monthly salary of {self.salamount}" + self.get_commission() + f"  Their total pay is {self.get_pay()}."
+        return message
 
 
 class HourlyEmployee(Employee):
     def __init__(self, name, hourwage, hours, commissiontype=None):
         super().__init__(name, commissiontype)
-        self.employeetype = "hourly"
+
         self.hourwage = hourwage
         self.hours = hours
+
+    def get_pay(self):
+        return (self.hourwage * self.hours) + self.add_commission()
+
+    def __str__(self):
+        message = f"{self.name} works on a contract of {self.hours} hours at {self.hourwage}/hour" + self.get_commission() + f"  Their total pay is {self.get_pay()}."
+        return message
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
@@ -87,21 +86,21 @@ charlie = HourlyEmployee('Charlie', 25, 100)
 charlie.__str__()
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = SalaryEmployee('Renee',3000, "contract")
-renee.add_contracts(4,200)
+renee = SalaryEmployee('Renee', 3000, "contract")
+renee.add_contracts(4, 200)
 renee.__str__()
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = HourlyEmployee('Jan',25,150,"contract")
-jan.add_contracts(3,220)
+jan = HourlyEmployee('Jan', 25, 150, "contract")
+jan.add_contracts(3, 220)
 jan.__str__()
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = SalaryEmployee('Robbie',2000,"bonus")
+robbie = SalaryEmployee('Robbie', 2000, "bonus")
 robbie.add_bonus(1500)
 robbie.__str__()
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = HourlyEmployee('Ariel',30,120,"bonus")
+ariel = HourlyEmployee('Ariel', 30, 120, "bonus")
 ariel.add_bonus(600)
 ariel.__str__()
